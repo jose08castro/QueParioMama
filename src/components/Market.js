e743c753f60976e3665ffc18b315e70d97469619
Evelio Castro <Jose Evelio>
2023-06-03 17:14:33 -0600

    Nuevo componente Market que hará de barra lateral

---
+++ src/components/Market.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaQuestion, FaIcon2, FaIcon3, FaIcon4, FaIcon5 } from 'react-icons/fa'; // Reemplaza FaIcon1, FaIcon2, etc. con los íconos de Font Awesome que elijas
+import './Market.css';
+
+function Market() {
+    return (
+      <div className="market-container">
+        {"Market"}
+      </div>
+    );
+  }
+
+export default Market;
